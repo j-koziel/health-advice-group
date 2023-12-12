1378ccc42d9323a91a67af15f9847744860b7f0b
j-koziel <jonathankoziel1@gmail.com>
2023-12-12 16:28:15 +0000

    feat(wip): air quality dashboard

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,16 +6,24 @@ import { Tabs, Tab } from "@nextui-org/react";
 
 export function Home() {
   const [weatherData, setWeatherData] = useState(null);
+  const [airQualityData, setAirQualityData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
     navigator.geolocation.getCurrentPosition((position) => {
-      const data = axios
+      const weatherData = axios
         .get(
           `https://api.openweathermap.org/data/3.0/onecall?units=metric&lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
         )
         .then((res) => setWeatherData({ ...res.data.current }))
+        .catch((err) => console.error(err))
+        .finally(() => setIsLoading(false));
+      const airQualityData = axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/air_pollution?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_AIR_QUALITY_KEY}`
+        )
+        .then((res) => setAirQualityData({ ...res.data.list[0] }))
         .catch((err) => console.error(err))
         .finally(() => setIsLoading(false));
     });
@@ -29,10 +37,9 @@ export function Home() {
           hello
         </Tab>
         <Tab key="air-quality" title="Air Quality">
-          <AirQualityDash />
+          {JSON.stringify(airQualityData)}
         </Tab>
       </Tabs>
-      {/* {JSON.stringify(weatherData)} */}
     </div>
   );
 }

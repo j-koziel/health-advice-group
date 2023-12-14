1cbfd803e348bc49092ff47f0a9c666b395b1945
j-koziel <jonathankoziel1@gmail.com>
2023-12-14 11:13:25 +0000

    feat: responsive header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,9 @@ import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export function Header() {
   return (
-    <div className="w-full h-[10vh] flex items-center justify-between px-4 bg-background text-foreground border-b-2 border-foreground">
-      <h1 className="text-2xl font-bold">Health Advice Group</h1>
-      <ChevronDown className="absolute left-0 right-0 mx-auto animate-bounce" />
+    <div className="relative w-full h-fit flex flex-col md:flex-row gap-2 items-center justify-between p-4 bg-background text-foreground border-b-2 border-foreground">
+      <h1 className="md:text-2xl text-lg font-bold">Health Advice Group</h1>
+      <ChevronDown className="animate-bounce !order-last md:absolute md:top-0 md:bottom-0 md:left-0 md:right-0 md:m-auto" />
       <ThemeSwitcher />
     </div>
   );

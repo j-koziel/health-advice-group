ffabd150f8d091a675b22679a742116c263a1db9
j-koziel <jonathankoziel1@gmail.com>
2024-01-10 17:45:00 +0000

    docs(backend): documenting db_utils functions

--- backend/db/db_utils.py
+++ backend/db/db_utils.py
@@ -1,12 +1,28 @@
 from json import load, dump
 
-def read_db(model, db_path: str):
+def read_db(model, db_path: str) -> list:
+  """This function loads the database and returns it as a python list
+
+  Args:
+    model: This is the model that will be used to validate the json data
+    db_path (str): This is the path to the database (json file)
+
+  Returns:
+    list: a list of python dictionaries which have been validated by the model
+  """
   with open(db_path) as json_file:
     return [model.model_validate_json(entity) for entity in load(json_file)]
   
 
-def save_db(model, db: list, db_path: str):
-  validated_db: list[model] = db
+def save_db(db: list, db_path: str) -> None:
+  """This function takes a new, updated database and inserts the new data into the json file
+  Args:
+    db (list): This is the new, updated db which is ready to be inserted into the json file
+    db_path (str): This is the path to the database (json file)
+
+  Returns:
+    None
+  """
   with open(db_path, "w") as json_file:
-    dump([entity.model_dump() for entity in validated_db], json_file, indent=4)
+    dump([entity.model_dump() for entity in db], json_file, indent=4)
 

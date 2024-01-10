dbd0734b46d81b57951e84ca34c00649485bebcb
j-koziel <jonathankoziel1@gmail.com>
2024-01-10 17:37:23 +0000

    feat: reading and saving the database

--- backend/db/db_utils.py
+++ backend/db/db_utils.py
@@ -1,5 +1,13 @@
+from json import load, dump
+from models import user_models
 
+def read_db(model, db_path: str):
+  with open(db_path) as json_file:
+    return [model.model_validate_json(entity) for entity in load(json_file)]
+  
 
+def save_db(model, db: list, db_path: str):
+  validated_db: list[model] = db
+  with open(db_path, "w") as json_file:
+    dump([entity.model_dump() for entity in validated_db], json_file, indent=4)
 
-def read_db(db: str):
-  return 

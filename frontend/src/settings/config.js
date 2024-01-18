e62b294b87a1f90d34be7a4422dc8ff4bc8bb8a1
j-koziel <jonathankoziel1@gmail.com>
2024-01-18 11:57:54 +0000

    feat(backend config): using env file to set backend url

--- frontend/src/settings/config.js
+++ frontend/src/settings/config.js
@@ -1,4 +1,5 @@
 export const config = {
+  backendUrl: process.env.REACT_APP_DEV_BACKEND_URL,
   weatherApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
   airQualityApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
 };

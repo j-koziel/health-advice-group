77d86200670ec8c405a2fb5a135e62b9c4462d71
j-koziel <jonathankoziel1@gmail.com>
2024-01-18 13:44:44 +0000

    fix(backend): using vercel env instead of node env

--- frontend/src/settings/config.js
+++ frontend/src/settings/config.js
@@ -1,8 +1,9 @@
 export const config = {
-  backendUrl:
-    process.env.NODE_ENV === "development"
-      ? process.env.REACT_APP_DEV_BACKEND_URL
-      : process.env.REACT_APP_PROD_BACKEND_URL,
+  deployedBackendUrl:
+    process.env.VERCEL_ENV === "production" ||
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PROD_BACKEND_URL
+      : process.env.REACT_APP_DEV_BACKEND_URL,
   weatherApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
   airQualityApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
 };

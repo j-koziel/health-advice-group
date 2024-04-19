64fdf48b889c3d54a0241e1525549e972f33367f
j-koziel <jonathankoziel1@gmail.com>
2024-04-19 10:47:10 +0100

    feat: added tooltips to air quality components

--- frontend/src/components/AirQualityDash.js
+++ frontend/src/components/AirQualityDash.js
@@ -1,4 +1,6 @@
 import { CircularProgress } from "@nextui-org/react";
+import { Tooltip } from "@nextui-org/react";
+
 import { AirQualityInfoModal } from "./AirQualityInfoModal";
 import { useDisclosure } from "@nextui-org/react";
 import { airQualityColour, airQualityDescription } from "../utils/air-quality";
@@ -71,143 +73,161 @@ export function AirQualityDash({
         <div className="w-full text-foreground flex flex-col justify-center items-center gap-8 md:flex-row">
           <div className="flex flex-col gap-y-4">
             <div className="flex">
-              <CircularProgress
-                value={airQualityData.components.pm2_5}
-                maxValue={75}
-                classNames={airComponentRingsStyles}
-                label="PM₂.₅"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.pm2_5,
-                  0,
-                  50,
-                  75
-                )}
-                formatOptions={{}}
-              />
-              <CircularProgress
-                value={airQualityData.components.pm10}
-                maxValue={200}
-                classNames={airComponentRingsStyles}
-                label="PM₁₀"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.pm10,
-                  0,
-                  50,
-                  200
-                )}
-                formatOptions={{}}
-              />
+              <Tooltip content="Particulate Matter">
+                <CircularProgress
+                  value={airQualityData.components.pm2_5}
+                  maxValue={75}
+                  classNames={airComponentRingsStyles}
+                  label="PM₂.₅"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.pm2_5,
+                    0,
+                    50,
+                    75
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
+              <Tooltip content="Particulate Matter">
+                <CircularProgress
+                  value={airQualityData.components.pm10}
+                  maxValue={200}
+                  classNames={airComponentRingsStyles}
+                  label="PM₁₀"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.pm10,
+                    0,
+                    50,
+                    200
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
             </div>
             <div className="flex">
-              <CircularProgress
-                value={airQualityData.components.so2}
-                maxValue={350}
-                classNames={airComponentRingsStyles}
-                label="SO₂"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.so2,
-                  0,
-                  250,
-                  350
-                )}
-                formatOptions={{}}
-              />
-              <CircularProgress
-                value={airQualityData.components.nh3}
-                minValue={0.1}
-                maxValue={200}
-                classNames={airComponentRingsStyles}
-                label="NH₃"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.nh3,
-                  0.1,
-                  100,
-                  200
-                )}
-                formatOptions={{}}
-              />
+              <Tooltip content="Sulfur dioxide">
+                <CircularProgress
+                  value={airQualityData.components.so2}
+                  maxValue={350}
+                  classNames={airComponentRingsStyles}
+                  label="SO₂"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.so2,
+                    0,
+                    250,
+                    350
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
+              <Tooltip content="Ammonia">
+                <CircularProgress
+                  value={airQualityData.components.nh3}
+                  minValue={0.1}
+                  maxValue={200}
+                  classNames={airComponentRingsStyles}
+                  label="NH₃"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.nh3,
+                    0.1,
+                    100,
+                    200
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
             </div>
           </div>
           <div>
-            <CircularProgress
-              value={airQualityData.main.aqi}
-              minValue={0}
-              maxValue={5}
-              classNames={airQualityIndexStyles}
-              label="AQI"
-              showValueLabel
-              color={airQualityColour(airQualityData.main.aqi, 1, 3, 5)}
-              formatOptions={{}}
-            />
+            <Tooltip content="Air Quality Index">
+              <CircularProgress
+                value={airQualityData.main.aqi}
+                minValue={0}
+                maxValue={5}
+                classNames={airQualityIndexStyles}
+                label="AQI"
+                showValueLabel
+                color={airQualityColour(airQualityData.main.aqi, 1, 3, 5)}
+                formatOptions={{}}
+              />
+            </Tooltip>
           </div>
           <div className="flex flex-col gap-y-4">
             <div className="flex">
-              <CircularProgress
-                value={airQualityData.components.co}
-                minValue={0}
-                maxValue={15400}
-                classNames={airComponentRingsStyles}
-                label="CO"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.co,
-                  0,
-                  12400,
-                  15400
-                )}
-                formatOptions={{}}
-              />
-              <CircularProgress
-                value={airQualityData.components.no}
-                minValue={0.1}
-                maxValue={100}
-                classNames={airComponentRingsStyles}
-                label="NO"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.no,
-                  0.1,
-                  50,
-                  100
-                )}
-                formatOptions={{}}
-              />
+              <Tooltip content="Carbon monoxide">
+                <CircularProgress
+                  value={airQualityData.components.co}
+                  minValue={0}
+                  maxValue={15400}
+                  classNames={airComponentRingsStyles}
+                  label="CO"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.co,
+                    0,
+                    12400,
+                    15400
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
+              <Tooltip content="Nitric oxide">
+                <CircularProgress
+                  value={airQualityData.components.no}
+                  minValue={0.1}
+                  maxValue={100}
+                  classNames={airComponentRingsStyles}
+                  label="NO"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.no,
+                    0.1,
+                    50,
+                    100
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
             </div>
             <div className="flex">
-              <CircularProgress
-                value={airQualityData.components.no2}
-                minValue={0}
-                maxValue={200}
-                classNames={airComponentRingsStyles}
-                label="NO₂"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.no2,
-                  0,
-                  150,
-                  200
-                )}
-                formatOptions={{}}
-              />
-              <CircularProgress
-                value={airQualityData.components.o3}
-                minValue={0}
-                maxValue={180}
-                classNames={airComponentRingsStyles}
-                label="O₃"
-                showValueLabel
-                color={airQualityColour(
-                  airQualityData.components.o3,
-                  0,
-                  140,
-                  180
-                )}
-                formatOptions={{}}
-              />
+              <Tooltip content="Nitrogen dioxide">
+                <CircularProgress
+                  value={airQualityData.components.no2}
+                  minValue={0}
+                  maxValue={200}
+                  classNames={airComponentRingsStyles}
+                  label="NO₂"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.no2,
+                    0,
+                    150,
+                    200
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
+              <Tooltip content="Ozone">
+                <CircularProgress
+                  value={airQualityData.components.o3}
+                  minValue={0}
+                  maxValue={180}
+                  classNames={airComponentRingsStyles}
+                  label="O₃"
+                  showValueLabel
+                  color={airQualityColour(
+                    airQualityData.components.o3,
+                    0,
+                    140,
+                    180
+                  )}
+                  formatOptions={{}}
+                />
+              </Tooltip>
             </div>
           </div>
         </div>

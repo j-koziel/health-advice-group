804fb5da8b42434dc8e97478dddc36aab6744883
j-koziel <jonathankoziel1@gmail.com>
2024-08-05 16:22:28 +0100

    feat: show ai generated health advice

--- frontend/src/components/HealthAdvice.js
+++ frontend/src/components/HealthAdvice.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Spinner } from "@nextui-org/spinner";
 
 import { getHealthAdviceData } from "../utils/get-data";
 
@@ -7,28 +8,54 @@ export function HealthAdvice({ weatherData }) {
 
   useEffect(() => {
     const getAndSetHealthAdviceData = async () => {
-      setHealthAdviceData({
-        ...(await getHealthAdviceData(weatherData)),
-      });
+      setHealthAdviceData(await getHealthAdviceData(weatherData));
     };
 
     getAndSetHealthAdviceData();
   }, []);
 
+  const extractAdviceHeading = (adviceString) => {
+    const firstOccurrenceIndex = adviceString.indexOf("**");
+    const secondOccurrenceIndex = adviceString.indexOf(
+      "**",
+      firstOccurrenceIndex + 1
+    );
+
+    return adviceString
+      .slice(firstOccurrenceIndex, secondOccurrenceIndex)
+      .replace("**", "");
+  };
+
+  const extractAdviceContent = (adviceString) => {
+    const firstOccurrenceIndex = adviceString.indexOf("**");
+    const secondOccurrenceIndex = adviceString.indexOf(
+      "**",
+      firstOccurrenceIndex + 1
+    );
+
+    return adviceString.slice(secondOccurrenceIndex).replace("**:", "");
+  };
+
   return (
-    <div className="flex flex-col items-center gap-y-4 md:items-start">
+    <div className="flex flex-col items-center gap-y-4 w-1/2">
       <h1 className="text-xl md:text-5xl font-bold">Our Advice:</h1>
-      {healthAdviceData && (
-        <ul className="flex flex-col items-center gap-y-12 text-lg md:text-4xl md:items-start">
-          <li>{healthAdviceData.uv_advice}</li>
-          <li>{healthAdviceData.temp_advice}</li>
-          <li>
-            {healthAdviceData.health_risks.length > 1
-              ? `There is a heightened risk of ${healthAdviceData.health_risks[0]}`
-              : healthAdviceData.health_risks[0]}
-          </li>
+      {healthAdviceData ? (
+        <ul className="flex flex-col items-center gap-y-12 text-lg">
+          {healthAdviceData.map((adviceString, i) => (
+            <li key={i}>
+              <strong>{extractAdviceHeading(adviceString)}:</strong>
+              {extractAdviceContent(adviceString)}
+            </li>
+          ))}
         </ul>
+      ) : (
+        <Spinner />
       )}
+      <p>
+        <strong className="text-danger">DISCLAIMER:</strong> This advice is
+        generated using ChatGPT so it is not verified and is mainly for
+        demonstration purposes
+      </p>
     </div>
   );
 }

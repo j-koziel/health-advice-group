1cfed40ec76730c33dc7838dd6f0a457b10228d2
j-koziel <jonathankoziel1@gmail.com>
2024-08-05 16:23:54 +0100

    ci: remove useless tests

--- frontend/src/tests/unit/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { Header } from "../../components/Header";
-import { render, screen } from "@testing-library/react";
-
-// Test to see whether the header title actually appears on the header
-test("renders page title in header", () => {
-  render(<Header />);
-  const footerHeader = screen.getByText("Health Advice Group");
-  expect(footerHeader).toBeInTheDocument();
-});

8e01cf76c93fbfbaf30382cf88143c178f954e04
j-koziel <jonathankoziel1@gmail.com>
2024-09-13 14:56:49 +0100

    chore: update config.js

--- frontend/src/settings/config.js
+++ frontend/src/settings/config.js
@@ -1,7 +1,5 @@
 export const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL,
-  weatherApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
-  airQualityApiKey: process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
   weatherApiUrl: `${process.env.REACT_APP_BACKEND_URL}/api/v1/weather`,
   airQualityApiUrl: `${process.env.REACT_APP_BACKEND_URL}/api/v1/air-quality`,
   reverseGeoLocationApiUrl: `${process.env.REACT_APP_BACKEND_URL}/api/v1/reverse-geo-location`,
